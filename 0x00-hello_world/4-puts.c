dcd6e20f76088b924a6d969262f5996ba136f1a5
Innovara-T <eibeabuchi2425a961csc@stu.cu.edu.ng>
2025-04-16 00:01:41 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *
+ * The return value should be Zero
+ *
+ */
+
+int main(){
+	char sentence[] = 'Programming is like building a multilingual puzzle';
+
+	puts("%s", sentence);
+
+	return(0);
