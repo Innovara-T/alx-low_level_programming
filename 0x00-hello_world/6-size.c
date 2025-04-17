66840b53a666385d6514b3ea3e32d8301657081e
Innovara-T <eibeabuchi2425a961csc@stu.cu.edu.ng>
2025-04-17 12:03:12 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
     printf("Size of long int: %d byte(s)\n", sizeof(long int));
     printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
     printf("Size of float: %d bytes\n", sizeof(float));
-    return (0);
+
+    return(0);
 }
