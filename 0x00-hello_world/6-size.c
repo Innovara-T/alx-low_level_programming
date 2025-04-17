6588d13b29935a477d2a6aafa96881c1ce398825
Innovara-T <eibeabuchi2425a961csc@stu.cu.edu.ng>
2025-04-17 09:16:17 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@ int main(void)
 {
     printf("Size of char: %d byte(s)\n", sizeof(char));
     printf("Size of int: %d byte(s)\n", sizeof(int));
-    printf("Size of double: %d byte(s)\n", sizeof(double));
     printf("Size of long int: %d byte(s)\n", sizeof(long int));
     printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
     printf("Size of float: %d bytes\n", sizeof(float));
